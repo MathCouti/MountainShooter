7be45a1296a3bc7645909f9cf936b850773a819d
Matheus Coutinho Da Silva <math_coutinho@hotmail.com>
2024-07-30 14:31:55 -0300

    ADDING PLAYER AND ENEMY

--- code/Level.py
+++ code/Level.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 import sys
 from cgitb import text
@@ -5,7 +7,7 @@ from cgitb import text
 import pygame.display
 
 from code import Entity
-from code.Const import COLOR_WHITE, WIN_HEIGHT
+from code.Const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 from code.EntityFactory import EntityFactory
 
 
@@ -15,8 +17,12 @@ class Level:
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
-        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))  # extend para mais de 1
+        self.entity_list.append(EntityFactory.get_entity('Player1'))  # append para 1 entidade
         self.timeout = 20000  # 20 segundos
+        if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
+            self.entity_list.append(EntityFactory.get_entity('Player2'))  # append para 1 entidade
+        pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
 
     def run(self):
         pygame.mixer.music.load(f'./asset/{self.name}.mp3')
@@ -33,20 +39,21 @@ class Level:
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
-
+                    pygame.quit()
+                    sys.exit()
+                if event.type == EVENT_ENEMY:
+                    choise = random.choice(('Enemy1', 'Enemy2'))
+                    self.entity_list.append(EntityFactory.get_entity(choise))
             # printed text
             self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s',
                             COLOR_WHITE, (10, 5))
-            self.level_text(14, f'fps: {clock.get_fps():.0f}',
-                            COLOR_WHITE, (10, WIN_HEIGHT - 35))
+            (self
+             .level_text(14, f'fps: {clock.get_fps():.0f}',
+                            COLOR_WHITE, (10, WIN_HEIGHT - 35)))
             self.level_text(14, f'entidades: {len(self.entity_list)}',
                             COLOR_WHITE, (10, WIN_HEIGHT - 20))
 
             pygame.display.flip()
-
-        pygame.quit()
-        sys.exit()
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
         text_font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
