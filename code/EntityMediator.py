7678636e9bf9ef97c06ee4cb60a538fb49fb2b98
Matheus Coutinho Da Silva <math_coutinho@hotmail.com>
2024-07-31 07:32:18 -0300

    creating meadiator pattern. enemys destruction.

---
+++ code/EntityMediator.py
@@ -0,0 +1,22 @@
+from code.Enemy import Enemy
+from code.Entity import Entity
+
+
+class EntityMediator:
+    @staticmethod
+    def __verify_collision_window(ent: Entity):  # __ é método privado que só funciona nessa classe
+        if isinstance(ent, Enemy): #verifica se a entidade é inimigo
+            if ent.rect.right < 0:
+                ent.health = 0
+
+    @staticmethod
+    def verify_collision(entity_list: list[Entity]):  # lista de entidades
+        for i in range(len(entity_list)):
+            test_entity = entity_list[i]
+            EntityMediator.__verify_collision_window(test_entity)
+
+    @staticmethod
+    def verify_health(entity_list: list[Entity]):  # lista de entidades
+        for ent in entity_list:
+            if ent.health <= 0:
+                entity_list.remove(ent)
--- code/Level.py
+++ code/Level.py
@@ -9,6 +9,7 @@ import pygame.display
 from code import Entity
 from code.Const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 from code.EntityFactory import EntityFactory
+from code.EntityMediator import EntityMediator
 
 
 class Level:
@@ -54,6 +55,10 @@ class Level:
                             COLOR_WHITE, (10, WIN_HEIGHT - 20))
 
             pygame.display.flip()
+
+            #Verifies
+            EntityMediator.verify_collision(entity_list=self.entity_list)
+            EntityMediator.verify_health(entity_list=self.entity_list)
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
         text_font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
