446927953b1b447585742e4e7d7c08d665e8bb18
Matheus Coutinho Da Silva <math_coutinho@hotmail.com>
2024-08-01 14:10:00 -0300

    creating collisions

--- code/Menu.py
+++ code/Menu.py
@@ -2,7 +2,7 @@ import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_YELLOW, COLOR_WHITE
+from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_YELLOW, C_WHITE
 
 
 class Menu():
@@ -19,14 +19,14 @@ class Menu():
             # DESEJANDO AS IMAGENS
             self.window.blit(source=self.surf,
                              dest=self.rect)  # dizendo pra atualizar a imagem da tela com o surft na posição do rect
-            self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
-                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
+                    self.menu_text(20, MENU_OPTION[i], C_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
                 else:
-                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+                    self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
             pygame.display.flip()
 
 
